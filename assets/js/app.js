8bc10a0d924648494ade41275318990a57942b0e
Talha Shah <syedt.in00@gmail.com>
2024-05-12 13:03:08 -0500

    modefied app.js &  index.html

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,3 @@
-function search(e) {
-  console.log(e.value)
-  // fetch`https://api.openweathermap.org/data/2.5/weather?q=${e.target.value}&appid=c9644e36e339884153aeb7e1e1d60248&units=metric`
-  //   .then(() => result.json())
-  //   .then(() => (data = result));
-  // console.log(data);
-}
 
 mainForm.addEventListener("submit", handleForm);
 

eb8afb1ec87407788e9f3e2cda8d94c23676aff5
Talha Shah <syedt.in00@gmail.com>
2024-05-15 22:38:34 +0500

    modified index.html and style.css

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,45 @@
-// Codebase Removed
+const searchInp = document.getElementById("searchInp");
+const searchBtn = document.getElementById("searchBtn");
+const locateMe = document.getElementById("locateMe");
+const API_KEY = "210eeabe1cac851c368047662c4815fd";
+
+async function getSearchWeather() {
+  if (searchInp.value !== "") {
+    try {
+      const result = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${searchInp.value}&appid=${API_KEY}`
+      );
+      const data = await result.json();
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  } else {
+    console.log("Please enter a city name!");
+  }
+}
+
+async function getLocationWeather() {
+  async function resolved(location) {
+    const latitude = location.coords.latitude;
+    const longitude = location.coords.longitude;
+    try {
+      const result = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?lon=${longitude}&lat=${latitude}&appid=${API_KEY}`
+      );
+      const data = await result.json();
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  function rejected() {
+    console.log("Access Denied!");
+  }
+
+  navigator.geolocation.getCurrentPosition(resolved, rejected);
+}
+
+locateMe.addEventListener("click", getLocationWeather);
+searchBtn.addEventListener("click", getSearchWeather);
